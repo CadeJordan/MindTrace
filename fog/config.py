70862a9e941dc7bfde41eb9d8cd247d777afdaef
CadeJordan <cade.m.jordan@gmail.com>
2026-03-02 15:24:33 -0800

    Working connection from model on nano to fog and phone

--- fog/config.py
+++ fog/config.py
@@ -8,3 +8,5 @@ url = os.getenv("INFLUX_URL", "http://localhost:8086")
 token = os.getenv("INFLUX_TOKEN")
 org = "MindTrace"
 bucket = "emotion_bucket"
+
+edge_ws_url = os.getenv("EDGE_WS_URL", "").strip() or None
